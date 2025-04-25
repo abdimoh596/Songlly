2151248656958d2bc5d9628e65ae87ce1b1aea46
abdimoh596 <107062181+abdimoh596@users.noreply.github.com>
2025-04-25 16:10:33 -0700

    functionality working, playback working, ui looks good, optimize further if necesssary

--- lib/screens/music_swipe_screen.dart
+++ lib/screens/music_swipe_screen.dart
@@ -4,6 +4,9 @@ import '../models/track.dart';
 import '../services/spotify_auth_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'home_screen.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class MusicSwipeScreen extends StatefulWidget {
   const MusicSwipeScreen({super.key});
@@ -17,11 +20,14 @@ class _MusicSwipeScreenState extends State<MusicSwipeScreen> {
   String? albumImageUrl;
   List<String?> artistNames = [];
   String albumName = "No Album Name";
+  bool isMuted = false;
 
   final spotifyAuth = SpotifyAuthService(
     clientId: dotenv.env['SPOTIFY_CLIENT_ID'],
     clientSecret: dotenv.env['SPOTIFY_CLIENT_SECRET'],
   );
+
+  final AudioPlayer _audioPlayer = AudioPlayer();
 
   @override
   void initState() {
@@ -29,13 +35,33 @@ class _MusicSwipeScreenState extends State<MusicSwipeScreen> {
     _getNextTrack();
   }
 
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  Future<String?> fetchPreviewUrl(String trackId) async {
+    final response = await http.get(
+      Uri.parse('https://spotify-preview-api-5f88.onrender.com/preview/$trackId'),
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['preview_url'];
+    } else {
+      return null;
+    }
+  }
+
   void _getNextTrack() async {
+    await _audioPlayer.stop();
+
     if (GlobalMusicData.instance.recommendedTracks.isNotEmpty) {
       final track = GlobalMusicData.instance.recommendedTracks.first;
 
       if (await spotifyAuth.isAlreadySaved(track)) {
         GlobalMusicData.instance.recommendedTracks.remove(track);
-        _getNextTrack(); // Skip already saved track
+        _getNextTrack();
         return;
       }
       GlobalMusicData.instance.recommendedTracks.remove(track);
@@ -43,6 +69,11 @@ class _MusicSwipeScreenState extends State<MusicSwipeScreen> {
       final imageUrl = await spotifyAuth.fetchAlbumImageUrl(track.albumId);
       final trackArtists = await spotifyAuth.getArtistName(track);
       final trackAlbum = await spotifyAuth.getAlbumName(track);
+
+      final previewUrl = await fetchPreviewUrl(track.id);
+      if (previewUrl != null && previewUrl.isNotEmpty) {
+        await _audioPlayer.play(UrlSource(previewUrl));
+      }
 
       setState(() {
         currentTrack = track;
@@ -52,11 +83,19 @@ class _MusicSwipeScreenState extends State<MusicSwipeScreen> {
       });
     } else {
       Navigator.pushAndRemoveUntil(
+        // ignore: use_build_context_synchronously
         context,
         MaterialPageRoute(builder: (context) => const HomeScreen()),
         (Route<dynamic> route) => false,
       );
     }
+  }
+
+  void _toggleMute() {
+    setState(() {
+      isMuted = !isMuted;
+      _audioPlayer.setVolume(isMuted ? 0.0 : 1.0);
+    });
   }
 
   void _handleSwipe(bool liked) {
@@ -112,15 +151,16 @@ class _MusicSwipeScreenState extends State<MusicSwipeScreen> {
                 ),
                 const SizedBox(height: 10),
                 Text(albumName,
-                    style: const TextStyle(fontSize: 20, color: Color.fromRGBO(121, 16, 83, 1.0)), textAlign: TextAlign.center, softWrap: true, maxLines: 2,),
+                    style: const TextStyle(fontSize: 20, color: Color.fromRGBO(121, 16, 83, 1.0)), textAlign: TextAlign.center, softWrap: true, maxLines: 1),
                 const SizedBox(height: 5),
                 Text(artistNames.join(', '),
-                    style: const TextStyle(fontSize: 25, color: Color.fromRGBO(121, 16, 83, 1.0)), textAlign: TextAlign.center, softWrap: true, maxLines: 2,),
-                const SizedBox(height: 40),
+                    style: const TextStyle(fontSize: 25, color: Color.fromRGBO(121, 16, 83, 1.0)), textAlign: TextAlign.center, softWrap: true, maxLines: 1),
+                const SizedBox(height: 45),
+
+                // Swipe buttons
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    // X Button
                     GestureDetector(
                       onTap: () => _handleSwipe(false),
                       child: Container(
@@ -135,8 +175,12 @@ class _MusicSwipeScreenState extends State<MusicSwipeScreen> {
                         ),
                       ),
                     ),
-                    const SizedBox(width: 70),
-                    // Check Button
+                    const SizedBox(width: 30),
+                    IconButton(
+                      icon: Icon(isMuted ? Icons.volume_off : Icons.volume_up, size: 40, color: Color.fromRGBO(255, 130, 4, 1)),
+                      onPressed: _toggleMute,
+                    ),
+                    const SizedBox(width: 30),
                     GestureDetector(
                       onTap: () => _handleSwipe(true),
                       child: Container(
